4ea724e5fce819f38985babdab0d798c67ea733a
Akin19 <asadujjamanakin2@gmail.com>
2024-08-27 01:17:33 +0600

    Last commit

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
@@ -11,6 +12,9 @@ const ShopContextProvider = (props) => {
   const [showSearch, setShowSearch] = useState(false);
 
   const [cartItems, setCartItems] = useState({});
+
+  const navigate = useNavigate();
+
   const addToCart = async (itemId, size) => {
     if (!size || size.length === 0) {
       toast.error("Select Product Size");
@@ -47,6 +51,28 @@ const ShopContextProvider = (props) => {
 
   useEffect(() => {}, [cartItems]);
 
+  const updateQuantity = async (itemId, size, quantity) => {
+    let cartData = structuredClone(cartItems);
+    cartData[itemId][size] = quantity;
+    setCartItems(cartData);
+  };
+
+  const getCartAmount = () => {
+    let totalAmount = 0;
+    for (const items in cartItems) {
+      let itemInfo = products.find((product) => product._id == items);
+      for (const item in cartItems[items]) {
+        try {
+          if (cartItems[items][item] > 0) {
+            totalAmount += itemInfo.price * cartItems[items][item];
+          }
+        } catch (error) {}
+      }
+    }
+
+    return totalAmount;
+  };
+
   const value = {
     products,
     currency,
@@ -58,6 +84,9 @@ const ShopContextProvider = (props) => {
     cartItems,
     addToCart,
     getCartCount,
+    updateQuantity,
+    getCartAmount,
+    navigate,
   };
   return (
     <ShopContext.Provider value={value}>{props.children}</ShopContext.Provider>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,78 @@
+import { assets } from "../assets/assets";
+import Title from "../components/Title";
+import NewsLetter from "../components/NewsLetter";
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <div className="text-2xl text-center pt-8 border-t">
+        <Title text1={"ABOUT"} text2={"US"} />
+      </div>
+      <div className="my-10 flex flex-col md:flex-row gap-16">
+        <img
+          className="w-full md:max-w-[450px]"
+          src={assets.about_img}
+          alt=""
+        />
+        <div className="flex flex-col justify-center gap-6 md:w-2/4 text-gray-600">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+            harum et minima reprehenderit neque quia dicta veritatis,
+            perspiciatis optio vero sed ipsa, quis, impedit provident excepturi
+            commodi placeat culpa laboriosam. Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Deleniti minus, at est accusamus vel,
+            doloremque facilis error illum minima sunt vero, modi suscipit enim?
+            Inventore odio dignissimos voluptates eum accusantium!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque
+            eius, iste facilis{" "}
+          </p>
+          <b className="text-gray-800">Our Misson</b>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores
+            ad expedita animi qui aliquam quibusdam dolor molestias in, illum,
+            corporis iusto nesciunt accusamus accusantium consequuntur
+            repudiandae culpa atque, voluptatibus architecto!
+          </p>
+        </div>
+      </div>
+
+      <div className="text-xl py-4">
+        <Title text1={"WHY"} text2={"CHOOSE US"} />
+      </div>
+      <div className="flex flex-col md:flex-row text-sm mb-20 ">
+        <div className="border px-10 md:px-16 py-18 sm:py-20 flex flex-col gap-5">
+          <b>Quality Assurance:</b>
+          <p className="text-gray-600">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum
+            quaerat architecto nam unde est, earum commodi assumenda accusamus
+            recusandae at fuga perferendis voluptatibus inventore praesentium
+            tempore, porro dolor provident doloremque.
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-18 sm:py-20 flex flex-col gap-5">
+          <b>Convenience:</b>
+          <p className="text-gray-600">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum
+            quaerat architecto nam unde est, earum commodi assumenda accusamus
+            recusandae at fuga perferendis voluptatibus inventore praesentium
+            tempore, porro dolor provident doloremque.
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-18 sm:py-20 flex flex-col gap-5">
+          <b>Exceptional Customer Service</b>
+          <p className="text-gray-600">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum
+            quaerat architecto nam unde est, earum commodi assumenda accusamus
+            recusandae at fuga perferendis voluptatibus inventore praesentium
+            tempore, porro dolor provident doloremque.
+          </p>
+        </div>
+      </div>
+
+      <NewsLetter />
+    </div>
+  );
 };
 
 export default About;
