531e65eead0206853e2e736df270ba909f281cc4
Akin19 <asadujjamanakin2@gmail.com>
2024-10-10 15:42:47 +0600

    Updated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,45 @@
-// eslint-disable-next-line no-unused-vars
-import React from "react";
+// // eslint-disable-next-line no-unused-vars
+// import React from "react";
+// import { Route, HashRouter } from "react-router-dom";
+// import Home from "./pages/Home";
+// import About from "./pages/About";
+// import Cart from "./pages/Cart";
+// import Collection from "./pages/Collection";
+// import Contact from "./pages/Contact";
+// import Login from "./pages/Login";
+// import Orders from "./pages/Orders";
+// import PlaceOrder from "./pages/PlaceOrder";
+// import Product from "./pages/Product";
+// import Navbar from "./components/Navbar";
+// import Footer from "./components/Footer";
+// import SearchBar from "./components/SearchBar";
+// import { ToastContainer } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+
+// const App = () => {
+//   return (
+//     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+//       <ToastContainer />
+//       <Navbar />
+//       <SearchBar />
+//       <HashRouter>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/about" element={<About />} />
+//         <Route path="/cart" element={<Cart />} />
+//         <Route path="/collection" element={<Collection />} />
+//         <Route path="/contact" element={<Contact />} />
+//         <Route path="/login" element={<Login />} />
+//         <Route path="/orders" element={<Orders />} />
+//         <Route path="/place-order" element={<PlaceOrder />} />
+//         <Route path="/product/:productId" element={<Product />} />
+//       </HashRouter>
+//       <Footer />
+//     </div>
+//   );
+// };
+
+// export default App;
+
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -13,7 +53,7 @@ import Product from "./pages/Product";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import SearchBar from "./components/SearchBar";
-import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
@@ -22,6 +62,7 @@ const App = () => {
       <ToastContainer />
       <Navbar />
       <SearchBar />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -33,6 +74,7 @@ const App = () => {
         <Route path="/place-order" element={<PlaceOrder />} />
         <Route path="/product/:productId" element={<Product />} />
       </Routes>
+
       <Footer />
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,27 @@
+// import { createRoot } from "react-dom/client";
+// import App from "./App.jsx";
+// import "./index.css";
+// import { BrowserRouter } from "react-router-dom";
+// import ShopContextProvider from "./context/ShopContext.jsx";
+
+// createRoot(document.getElementById("root")).render(
+//   <BrowserRouter>
+//     <ShopContextProvider>
+//       <App />
+//     </ShopContextProvider>
+//   </BrowserRouter>
+// );
+
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import ShopContextProvider from "./context/ShopContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <ShopContextProvider>
       <App />
     </ShopContextProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
